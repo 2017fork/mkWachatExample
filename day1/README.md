4c1b8b80d9571f284735805716a1a656c6e597a4
qinhailang <qhl@qinhailangdeMac-mini.local>
2017-12-18 20:18:15 +0800

    day1

---
+++ day1/README.md
@@ -0,0 +1,27 @@
+## node.js 开发微信公众号
+
+### 环境配置
+   1. 微信测试账号： https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login
+   2. 内外穿透：https://www.ngrok.cc （有免费的，就是比较慢）
+
+#### 内外穿透：
+  *  根据 https://www.ngrok.cc 下载对应的客户端，根据官网配置域名和端口已经运行。
+
+     `需要注意： 本地端口如：127.0.0.1:1234，端口号为1234，要对应程序监听的端口app.listen(1234);`
+
+#### 微信测试账号(接口配置信息)：
+   * URL 设置为你在（内外穿透）设置的域名，或者也可以在运行ngrok的命令窗口上找到域名，
+   * Token 随意设置
+
+
+   > 如果接口配置信息提交失败，请查看ngrok是否启动且在线、app.listen(1234)端口和ngrok配置的本地端口是否一样、接口配置信息的URL和ngrok的域名是否一样。
+
+## 目录结构介绍
+
+```js
+|--- mkWachatExample
+    |--- day1 // 第一天
+        |--- app.js // 入口文件
+        |--- package.json
+        |--- README.md
+```
---
+++ day1/app.js
@@ -0,0 +1,43 @@
+/* ---验证公众号---
+* 微信接口绑定域名配置：
+*    1.将token、timestamp、nonce三个参数进行字典排序
+*    2.把字典排序的结果进项sha1加密
+*    3.将加密后的字符串与signature(签名)对比(相等则为微信请求，才能绑定)。原样返回就可以接入成功
+* */
+
+const Koa = require('koa');
+const sha1 = require('sha1');
+
+const config = {
+    wechat: {
+        appID: 'wxfad1f2a72bcbabd5',
+        appSecret: '537855617a4dcda24f42dd66677a9290',
+        token: 'langge',
+    }
+};
+
+const app = new Koa();
+app.use(function* (next) { // 迭代器生成器
+    /*
+    *  验证逻辑(开发者身份)
+    * */
+    const token = config.wechat.token;
+    const signature = this.query.signature; // 签名
+    const nonce = this.query.nonce;// 随机数
+    const timestamp = this.query.timestamp;// 时间戳
+    const echostr = this.query.echostr;
+    // 字典排序
+    const src = [token, timestamp, nonce].sort().join('');
+    const sha = sha1(src);
+
+    // 判断返回值是不是等于签名值(是否微信请求)
+    if (sha === signature) {
+        this.body = echostr + '';
+    } else {
+        this.body = 'wrong';
+    }
+
+});
+
+app.listen(1234);
+console.log('yes: 1234');
